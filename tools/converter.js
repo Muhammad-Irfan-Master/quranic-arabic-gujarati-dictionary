98c223c53bca938b72c5b7f3d2a3d473dcc07f4b
Muhammad-Irfan-Master <muirfanip@gmail.com>
2026-07-23 22:33:07 +0530

    Update converter.js

--- tools/converter.js
+++ tools/converter.js
@@ -1,7 +1,43 @@
-document
-.getElementById("convertBtn")
-.addEventListener("click",()=>{
+const fileInput = document.getElementById("excelFile");
+const button = document.getElementById("convertBtn");
+const status = document.getElementById("status");
 
-document.getElementById("status").innerHTML="Reading Excel...";
+button.addEventListener("click", () => {
+
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert("پہلے Excel فائل منتخب کریں");
+        return;
+    }
+
+    status.innerHTML = "Reading Excel...";
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+
+        const data = new Uint8Array(e.target.result);
+
+        const workbook = XLSX.read(data,{type:"array"});
+
+        const sheetName = workbook.SheetNames[0];
+
+        const worksheet = workbook.Sheets[sheetName];
+
+        const json = XLSX.utils.sheet_to_json(worksheet);
+
+        status.innerHTML =
+        `
+        ✅ Rows Read : ${json.length}
+        <br><br>
+        Sheet : ${sheetName}
+        `;
+
+        console.log(json);
+
+    };
+
+    reader.readAsArrayBuffer(file);
 
 });
