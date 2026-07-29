1774e856eab3756853534d4690e290c735c23c38
Muhammad-Irfan-Master <muirfanip@gmail.com>
2026-07-29 16:08:23 +0530

    Create surahs.js

---
+++ js/surahs.js
@@ -0,0 +1,49 @@
+fetch("data/database.json")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+const surahs={};
+
+data.forEach(item=>{
+
+if(!surahs[item.chapter]){
+
+surahs[item.chapter]=item.surah_name;
+
+}
+
+});
+
+let html="";
+
+Object.keys(surahs).forEach(chapter=>{
+
+html+=`
+
+<a href="surah.html?chapter=${chapter}"
+
+class="surah-card">
+
+<span class="surah-number">
+
+${chapter}
+
+</span>
+
+<span class="surah-name">
+
+${surahs[chapter]}
+
+</span>
+
+</a>
+
+`;
+
+});
+
+document.getElementById("surahList").innerHTML=html;
+
+});
